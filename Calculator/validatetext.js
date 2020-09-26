1954d1aa4095c1c6ace08094a5fff1769eea4c7c
Kumar <mohitkr.mnnit@gmail.com>
2020-09-27 00:14:23 +0530

    added stylesheet

--- Calculator/validatetext.js
+++ Calculator/validatetext.js
@@ -1,10 +1,10 @@
 function validate(evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if(charCode !=46 && charCode > 31
-        && (charCode < 42 || charCode > 57)) {
-            return false;
-        }
-    if(charCode == 46 && evt.srcElement.value.split('.').length>1){
+    if (charCode != 46 && charCode > 31 &&
+        (charCode < 42 || charCode > 57)) {
+        return false;
+    }
+    if (charCode == 46 && evt.srcElement.value.split('.').length > 1) {
         return false;
     }
 
